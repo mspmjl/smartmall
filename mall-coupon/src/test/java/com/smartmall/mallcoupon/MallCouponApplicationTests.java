a9ce34a472f6e4cd200582647db630515f5b3fd8
mspmjl <867181563@qq.com>
2020-10-22 20:56:03 +0800

    first time push

---
+++ mall-coupon/src/test/java/com/smartmall/mallcoupon/MallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartmall.mallcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
