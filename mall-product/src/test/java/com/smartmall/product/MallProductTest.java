de58987ef0b2ef42753f7711bcf940536657d0f9
mspmjl <867181563@qq.com>
2020-11-30 21:53:11 +0800

    push remove access key

--- mall-product/src/test/java/com/smartmall/product/MallProductTest.java
+++ mall-product/src/test/java/com/smartmall/product/MallProductTest.java
@@ -34,7 +34,7 @@ public class MallProductTest {
         // Endpoint以杭州为例，其它Region请按实际情况填写。
        /* String endpoint = "oss-cn-hangzhou.aliyuncs.com";
         // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
-        String accessKeyId = "LTAI4GHNxn9vR8vY697o8EKA";
+        String accessKeyId = "";
         String accessKeySecret = "1GPg1sGSyBIg4EjqjY1pEAImy3P9Kw";
 
         // 创建OSSClient实例。*/
